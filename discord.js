017b41cb5293c56b27110a50f04609da8db78c74
teramotodaiki <i101323@gmail.com>
2023-03-27 11:30:47 +0900

    log

--- discord.js
+++ discord.js
@@ -16,7 +16,7 @@ export async function DiscordRequest(endpoint, options) {
   // throw API errors
   if (!res.ok) {
     const data = await res.json();
-    console.log(res.status);
+    console.log(res.status, url);
     throw new Error(JSON.stringify(data));
   }
   // return original response
